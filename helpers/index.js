f7850b9b44edc4a0edb245a046f159d6ffcbda27
Maxime Bartier <poulycroc.studio@gmail.com>
2021-09-22 18:31:37 +0200

    add isNil helper

--- helpers/index.js
+++ helpers/index.js
@@ -1,12 +1,28 @@
 /**
+ * Check if element is null or undefined
+ * @param {*} any - String to convert
+ * @return {Boolean}
+ */
+const isNil = (any) => {
+  return (
+    any === undefined || any === null || any === "undefined" || any === "null"
+  );
+};
+
+/**
  * Convert CamelCaseString to snake_case
  * @param {String} str - String to convert
  * @return {String}
  */
-export const toSnakeCase = (str) => {
+const toSnakeCase = (str) => {
   if (isNil(str)) return null;
   return str
     .replace(/([a-z])([A-Z])/g, "$1_$2")
     .replace(/\s+/g, "_")
     .toLowerCase();
+};
+
+export default {
+  isNil,
+  toSnakeCase,
 };

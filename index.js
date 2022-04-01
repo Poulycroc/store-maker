c8ee3d8a2fa6660cd4f912de48127d7781523c21
Maxime Bartier <poulycroc.studio@gmail.com>
2022-04-01 18:34:56 +0200

    wip

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const isNil = (any) => {
  * @return {String}
  */
 const toSnakeCase = (str) => {
-  console.log({ toSnakeCase: str });
   if (isNil(str)) return null;
   return str
     .replace(/([a-z])([A-Z])/g, "$1_$2")
